a64f4a7db7982718c9a26b8047cadc71d056ec42
Tony Wang <yilongwang05@gmail.com>
2026-08-04 11:39:11 -0700

    fix(ci): define PagesFunction type in functions/api/[[path]].ts to pass typecheck

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -2,6 +2,15 @@ interface Env {
   BACKEND_API_URL?: string;
 }
 
+type PagesFunction<Env = any> = (context: {
+  request: Request;
+  env: Env;
+  params?: Record<string, string | string[]>;
+  waitUntil?: (promise: Promise<any>) => void;
+  next?: (input?: RequestInfo, init?: RequestInit) => Promise<Response>;
+  data?: Record<string, any>;
+}) => Promise<Response> | Response;
+
 export const onRequest: PagesFunction<Env> = async (context) => {
   const { request, env } = context;
   const url = new URL(request.url);
