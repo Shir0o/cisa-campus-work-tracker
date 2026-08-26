6427143fa246157c4666f6dd0640be10bebcdf40
Tony Wang <Shir0o@users.noreply.github.com>
2026-08-26 09:44:51 -0700

    feat: localize coordination note cards and dynamic content in Spanish (#586)

--- server.ts
+++ server.ts
@@ -2024,8 +2024,8 @@ CRITICAL RULES:
   } else {
     const distPath = path.join(process.cwd(), "dist");
     app.use(express.static(distPath));
-    app.get(/.*/, (req, res) => {
-      res.sendFile(path.join(distPath, "index.html"));
+    app.use((req, res) => {
+      res.sendFile("index.html", { root: distPath });
     });
   }
 
